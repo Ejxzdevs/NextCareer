03d5f5abfaca4d4ddd46478f70b5f6ff7da1aa6b
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-04-13 03:15:25 +0800

    init

---
+++ resources/js/components/navbar.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <nav>
+        <Link href="/">Login</Link>
+        <Link href="/Register">Register</Link>
+    </nav>
+</template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="home">
-        
         <h1>Welcome to NextCareer</h1>
         <p>This is a simple Vue.js application.</p>
     </div>
