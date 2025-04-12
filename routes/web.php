fe6dfe79f0b31086ab4dcc625de71827e6780175
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-04-13 03:03:08 +0800

    feat: create route for user regsitration

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return inertia::render('Login');
-});
+Route::inertia('/', 'Login')->name('login');
+Route::inertia('/Register', 'Register')->name('register');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-Route::get('/Register', function () {
-    return inertia::render('Register');
-});
