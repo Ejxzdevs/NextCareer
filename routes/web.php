daa1ca76d72141d768a07bfda0ff14716d515f85
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-04-30 01:27:06 +0800

    feat: add logout route for user session termination

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\UserController;
 
 
 Route::inertia('/', 'Auth/Login')->name('login');
 Route::inertia('/register', 'Auth/Register')->name('register');
-Route::inertia('/home', 'Home')->name('home');
 Route::post('/users/authenticate', [UserController::class, 'authenticate'])->name('users.authenticate');
 Route::post('/users/create', [UserController::class, 'store'])->name('users.store');
 
+Route::inertia('employer/dahsboard', 'Employer/Dashboard')->name('employer.dashboard');
+Route::inertia('freelance/home', 'Freelance/Home')->name('freelance.home');
+
+
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
