1474dc0498b3e385f15b17a772c0612b234df18f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-04-30 01:24:45 +0800

    Add logout method to controller to handle user logout and session invalidation

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -39,10 +39,22 @@ class userController extends Controller
         Session::put('user_id', Auth::id());
         Session::put('user_role', Auth::user()->account_type);
 
-        return redirect()->route('home');
+        if(Auth::user()->account_type == 'employer'){
+            return redirect()->route('employer.dashboard');
+        } elseif(Auth::user()->account_type == 'freelance'){
+            return redirect()->route('freelance.home');
+        }
     } else {
         return redirect()->back()->with('error', 'Invalid credentials!');
     }
 
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+    
+        return redirect()->route('login');
     }
 }
