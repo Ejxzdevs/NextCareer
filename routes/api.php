4744cde8355f9abea36370cfec3e4dd71117c456
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-06-19 03:17:35 +0800

    feat: create route for MarkasReadNotifications

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
 
 // Routes in api.php automatically get the '/api' prefix.
-// So, this route will be accessible at: your-app.com/api/notifications/raw-projects
 Route::get('/notifications/raw-projects', [NotificationController::class, 'getUserNotifications'])->name('api.raw.notifications');
+Route::post('/notifications/mark-as-viewed', [NotificationController::class, 'markApplicationsAsViewed'])->name('update.applications.viewed');
