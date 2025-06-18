df39043370a3bb22fe38eb093ab67a28db777bb1
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-06-19 06:00:01 +0800

    fix: change notifications mark-as-viewed route method from POST to PUT

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\NotificationController;
 
 // Routes in api.php automatically get the '/api' prefix.
 Route::get('/notifications/raw-projects', [NotificationController::class, 'getUserNotifications'])->name('api.raw.notifications');
-Route::post('/notifications/mark-as-viewed', [NotificationController::class, 'markApplicationsAsViewed'])->name('update.applications.viewed');
+Route::put('/notifications/mark-as-viewed', [NotificationController::class, 'markApplicationsAsViewed'])->name('update.applications.viewed');
