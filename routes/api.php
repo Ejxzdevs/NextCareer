51e5e1f0a7dd9a120db3e11ca6f1664ccfb904f0
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-06-22 03:39:37 +0800

    fix(routes): apply web middleware to support all HTTP methods

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
 
 // Routes in api.php automatically get the '/api' prefix.
+Route::middleware('web')->group(function () {
 Route::get('/notifications/raw-projects', [NotificationController::class, 'getUserNotifications'])->name('api.raw.notifications');
 Route::put('/notifications/mark-as-viewed', [NotificationController::class, 'markApplicationsAsViewed'])->name('update.applications.viewed');
+});
