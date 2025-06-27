c09a5a52b3d03236974dd40b816db6410b79ec6c
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-06-28 07:41:56 +0800

    init

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,12 @@ import { createApp , h} from 'vue'
 import { createInertiaApp, Link, Head } from '@inertiajs/vue3'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy'
 import '@fortawesome/fontawesome-free/css/all.css';
-import MainLayout from './Pages/Layouts/MainLayout.vue';
+import MainLayout from './pages/Layouts/MainLayout.vue';
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
+    const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
+    return pages[`./pages/${name}.vue`]
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
