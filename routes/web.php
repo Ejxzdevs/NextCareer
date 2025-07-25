d8f85ecb47e2021b15143423c3d907927007288d
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-07-25 18:01:07 +0800

    feat: make application_id optional in project.show route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\DashboardController;
 
 Route::inertia('/', 'LandingPage')->name('landing.page');
 Route::inertia('/Login', 'Auth/Login')->name('login');
@@ -12,10 +13,11 @@ Route::post('/users/authenticate', [UserController::class, 'authenticate'])->nam
 Route::post('/users/create', [UserController::class, 'store'])->name('users.store');
 
 
-Route::inertia('employer/dahsboard', 'Employer/Dashboard')->name('employer.dashboard');
+Route::get('employer/dahsboard', [DashboardController::class, 'index'])->name('employer.dashboard');
 Route::get('/employer/project', [ProjectController::class, 'index'])->name('employer.project');
+Route::inertia('employer/application', 'Employer/Application')->name('employer.application');
 Route::post('/employer/create', [ProjectController::class, 'store'])->name('employer.store');
-Route::get('/employer/project/{id}/{application_id}', [ProjectController::class, 'show'])->name('project.show');
+Route::get('/employer/project/{id}/{application_id?}', [ProjectController::class, 'show'])->name('project.show');
 Route::put('/employer/{id}', [ProjectController::class, 'update'])->name('employer.update');
 Route::delete('/employer/{id}', [ProjectController::class, 'destroy'])->name('employer.destroy');
 
