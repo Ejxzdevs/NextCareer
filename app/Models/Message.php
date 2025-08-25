69fda6f9319be9802981889f8fd06a38b43c601b
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-08-26 02:44:21 +0800

    feat(message): create Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'message_id';
+
+    // Mass assignable fields
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'message_content',
+        'status',
+    ];
+
+    // Relationships
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
