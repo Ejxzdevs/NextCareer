f6c9e27ecbd3498748783c80a2b7e424df7a5d74
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-08-27 04:20:52 +0800

    feat: Add getMessageNotifications method to retrieve message notifications list

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,24 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
+    // Get message notifications
+    public function getMessageNotifications()
+    {
+        $authId = Auth::id();
+
+        $users = User::withWhereHas('receivedMessages', function ($q) use ($authId) {
+        $q->where('receiver_id', $authId)
+          ->latest()
+          ->take(1)->with(['sender']);
+        })
+        ->get();
+            return response()->json($users);
+    }
+
     // Send a message
      public function sendMessage(Request $request)
     {
 
-        $userId = auth::id();
+        $sender_id = auth::id();
         $request->validate([
             'receiver_id' => 'required|exists:users,id',
             'message_content' => 'required|string',
         ]);
 
         $message = Message::create([
-            'sender_id' =>  $userId,
+            'sender_id' =>  $sender_id,
             'receiver_id' => $request->receiver_id,
             'message_content' => $request->message_content
         ]);
