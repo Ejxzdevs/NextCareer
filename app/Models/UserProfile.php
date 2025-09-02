b36e789a09adb8c2a3f54c81353521b82b5827a6
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-09-03 07:17:37 +0800

    feat: add user_id to fillable

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -7,15 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class UserProfile extends Model
 {
     protected $fillable = [
+        'user_id',        // add this!
         'address',
         'skills',
         'occupation',
         'about',
         'profile_picture',
+        'user_status',    // include if you want to set it later
     ];
+    
     protected $casts = [
         'skills' => 'array',
     ];
+    
       public function user()
     {
         return $this->belongsTo(User::class);
