71bcf6fbf8d3b118fc7e6bc8b8802c0ddd0923cf
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-09-03 07:19:25 +0800

    refactor: remove import model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use App\Models\Message;
-
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -46,6 +44,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+      public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
     }
 
      public function sentMessages(): HasMany
