e6fae5957126ce8f6adf876c2f0fde7f9bc5c7dc
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-09-03 19:20:50 +0800

    refactor: improve route names for clarity and readability

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,24 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\MessageController;
 
+
+// Landing Page Route
 Route::inertia('/', 'LandingPage')->name('landing.page');
+
+// show login and register pages
 Route::inertia('/Login', 'Auth/Login')->name('login');
 Route::inertia('/register', 'Auth/Register')->name('register');
+
+// logout route
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+// user authentication and creation routes
 Route::post('/users/authenticate', [UserController::class, 'authenticate'])->name('users.authenticate');
 Route::post('/users/create', [UserController::class, 'store'])->name('users.store');
 
-
-Route::get('employer/dahsboard', [DashboardController::class, 'index'])->name('employer.dashboard');
+// employer
+Route::get('employer/dashboard', [DashboardController::class, 'index'])->name('employer.dashboard');
 Route::get('/employer/project', [ProjectController::class, 'index'])->name('employer.project');
 Route::get('employer/application', [ApplicationController::class, 'index'])->name('employer.application');
 Route::post('/employer/create', [ProjectController::class, 'store'])->name('employer.store');
@@ -23,18 +31,18 @@ Route::get('/employer/project/{id}/{application_id?}', [ProjectController::class
 Route::put('/employer/{id}', [ProjectController::class, 'update'])->name('employer.update');
 Route::delete('/employer/{id}', [ProjectController::class, 'destroy'])->name('employer.destroy');
 
+// freelance
 Route::inertia('freelance/home', 'Freelance/Home')->name('freelance.home');
 Route::get('freelance/browse', [ProjectController::class, 'index'])->name('freelance.browse');
 Route::post('freelance/application', [ApplicationController::class, 'store'])->name('freelance.application');
 
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+// shared routes
+// go to all notifications page
+Route::inertia('user/all/notifications', 'Employer/AllNotifications')->name('user.allNotifications.index');
+Route::inertia('user/all/messages','Employer/AllMessages')->name('user.AllMessages.index');
 
-Route::inertia('/notifications', 'Employer/AllNotifications')->name('notifications.all');
+// user profile routes
 Route::get('user/profile/{id}', [ProfileController::class,'showProfile'])->name('userProfile.show');
-Route::put('/user/profile', [ProfileController::class, 'update'])->name('userProfile.update');
+Route::put('/user/profile', [ProfileController::class, 'updateProfile'])->name('userProfile.update');
 
-Route::post('/messages/send', [MessageController::class, 'sendMessage'])->name('messages.send');
-Route::get('/messages/notifications', [MessageController::class, 'getMessageNotifications'])->name('messages.listWithLastMessage');
-Route::get('/messages/all',[MessageController::class, 'getAllMessages'])->name('messages.all');
-Route::get('/messages/conversation/{userId}', [MessageController::class, 'conversation'])->name('messages.conversation');
-Route::put('/messages/mark-all-as-read', [MessageController::class, 'markAllAsRead'])->name('messages.markAllAsRead');
+
