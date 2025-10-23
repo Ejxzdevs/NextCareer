cdb2dffe351c61fbc03c48c502885eccbbc297d9
Ejxzdevs <johnerickgofredo163@gmail.com>
2025-10-23 14:27:09 +0800

    feat: add Echo import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,23 @@
-import '../css/app.css'
-import { createApp , h} from 'vue'
-import { createInertiaApp, Link, Head } from '@inertiajs/vue3'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy'
-import '@fortawesome/fontawesome-free/css/all.css';
-import MainLayout from './pages/Layouts/MainLayout.vue';
+import "../css/app.css";
+import "./echo";
+import { createApp, h } from "vue";
+import { createInertiaApp, Link, Head } from "@inertiajs/vue3";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import "@fortawesome/fontawesome-free/css/all.css";
+import MainLayout from "./pages/Layouts/MainLayout.vue";
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
-    return pages[`./pages/${name}.vue`]
-  },
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(ZiggyVue)
-      .component('Link', Link)
-      .component('Head', Head)
-      .component('MainLayout', MainLayout)
-      .mount(el)
-  },
-})
+    resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.vue", { eager: true });
+        return pages[`./pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .component("Link", Link)
+            .component("Head", Head)
+            .component("MainLayout", MainLayout)
+            .mount(el);
+    },
+});
